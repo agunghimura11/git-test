084a96e3bd0fc327f685a20c21eae670316faf67
agunghimura11 <agung.arisudana@gmail.com>
2020-10-05 11:59:34 +0800

    Penambahan style.css

--- index.php
+++ index.php
@@ -4,5 +4,10 @@
     <h1>Latihan 1</h1>
 
     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+    <ul>
+        <li>1. Poin 1</li>
+        <li>1. Poin 1</li>
+        <li>1. Poin 1</li>
+    </ul>
 </body>
 </html>
